8c29de5b2b2311d977b8b07d1c2a07cb44e4aa96
Antonio Lerario <amlerario@gmail.com>
2014-07-20 10:27:05 -0400

    Changed variable names

--- mean.py
+++ mean.py
@@ -6,7 +6,7 @@ n=0
 # modify the variable name
 # adding comments just to test
 for num in open('data.txt'): #open as existing file directely
-	sum += float(num)
-	n += 1
+	addition += float(addition) ### changed the name of this variable
+	i += 1 ### changed the name of this variable too
 
-print sum/n
+print addition/i ### upfdated with the new variables
